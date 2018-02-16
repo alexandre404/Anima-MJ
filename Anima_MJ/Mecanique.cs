dbbeae7bc2e50791d9564bf99e355f5d3676f6c2
alexandre404 <dragnen404@gmail.com>
2018-02-16 12:18:15 +0100

    Test sur la base de données dans la table level Commantaires dans mécaniques.cs et test de la fonctions CalculLevel

--- Anima_MJ/Mecanique.cs
+++ Anima_MJ/Mecanique.cs
@@ -4,38 +4,39 @@ using System.Linq;
 namespace Anima_MJ
 {
     
-
+    //classe dédié à toutes les fonctions linq 
+    // c'est à dire toutes les fonctions qui interragissent avec les tables
     public static  class Mecanique
     {
+        //Fonction gerant la valeur du niveaux suivant lors d'un level up
     public static int CalculLevel(Outils.Perso perso)
         {
-            int level;
+            int c = 0;
+            int level = 1;
             var query = from a in Donnees.Db.Level.Take(1)
                         where a.id_joueur == perso.Id
                         orderby a.level1 descending
                         select a.level1;
             foreach (var a in query)
             {
-                if (query != null)
-                {
+                c++;
                     level = Convert.ToInt32(query) + 1;
-                    return level;
+                  
+
+            }
+            if (c == 0)
+            {
+                if (Data.level1 == true)
+                {
+                    level = 1;
+                    
                 }
                 else
                 {
-                    if (Data.level1 == true)
-                    {
-                        level = 1;
-                        return level;
-                    }
-                    else
-                    {
-                        level = 0;
-                        return level;
-                    }
+                    level = 0;
                     
                 }
-                
+
             }
             return level;
         }
